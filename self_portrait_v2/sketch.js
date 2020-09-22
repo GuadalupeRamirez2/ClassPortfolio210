4edc5bcd4035f08e7cb40327164ddf6a758f4c7f
GuadalupeRamirez2 <64633227+GuadalupeRamirez2@users.noreply.github.com>
2020-09-22 18:40:46 -0400

    updated

--- self_portrait_v2/sketch.js
+++ self_portrait_v2/sketch.js
@@ -6,6 +6,7 @@ by guadalupe ramirez
 //global scope
 var x = 315; // character origin
 var y = 210;
+var faceSize = 200;
 
 function setup() {
     createCanvas(500, 400);
@@ -19,26 +20,26 @@ function draw() {
 
     //face
     fill(162, 218, 24);
-    square(x-110, y-60, 200, 20);//head
+    square(x-110, y-60, faceSize);//head
     //eyes
     fill(255, 255, 255);
-    circle(x-55, y+10, 90); // left
+    circle(x-55, y+10, faceSize/2); // left
     
-    circle(x+65, y+10, 90); //right
+    circle(x+65, y+10, faceSize/2); //right
     fill(85, 62, 57);
-    circle(x-45, y+10, 50); // left brown eyeball
+    circle(x-45, y+10, faceSize/4); // left brown eyeball
     fill(85, 62, 57);
-    circle(x+75, y+10, 50); //right brown eyeball
+    circle(x+75, y+10, faceSize/4); //right brown eyeball
     //nose
     fill(85, 62, 57);
-    rect(x+5, y-40, 10, 100); //nose
+    rect(x+5, y-40, 10, faceSize/2); //nose
     //mouth
     fill(218, 200, 24);
-    rect(x-85, y+90, 80, 20);//lips
+    rect(x-85, y+90, faceSize/2-20,faceSize/10);//lips
 
     //hat
     fill(24, 218, 182);
-    rect(x-115, y-90, 200, 30);
+    rect(x-115, y-90, faceSize, 30);
     
     circle(x, y, 0);
 }

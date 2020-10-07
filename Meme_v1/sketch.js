8bcd2a5c7dd9f2da17ea273e12e6f6c06499dd1a
GuadalupeRamirez2 <64633227+GuadalupeRamirez2@users.noreply.github.com>
2020-10-07 19:11:37 -0400

    add image

---
+++ Meme_v1/sketch.js
@@ -0,0 +1,41 @@
+/*
+meme V 1
+by guadalupe ramirez
+text + images
+*/
+
+var voiceImage;
+
+function preload() {
+    voiceImage = loadImage('voicememe.jpg');
+}
+
+//var hangImage;
+
+//function preload() {
+    //hangImage = loadImage('hangup.png');
+//}
+
+
+function setup() {
+    var canvas = createCanvas(840, 460);
+    canvas.drawingContext.miterLimit = 2;
+}
+
+function draw() {
+    background('lightblue');
+
+    /* image */
+    image(voiceImage, 110, 94);
+
+    /*hang up button*/
+    //image(hangImage, width / 2, 100);
+
+    textSize(35);
+    fill('gold');
+    fill('gold');
+    stroke('black');
+    strokeWeight(10);
+    text('Professor: Okay guys, thats it for to...', 35, 60);
+    text('Me:', 35, 100);
+}

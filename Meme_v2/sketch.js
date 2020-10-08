af6798eb0cea9858a62272da2168bad03c586fd0
GuadalupeRamirez2 <64633227+GuadalupeRamirez2@users.noreply.github.com>
2020-10-08 19:19:44 -0400

    still working on it

--- Meme_v2/sketch.js
+++ Meme_v2/sketch.js
@@ -1,11 +1,11 @@
 /*
-meme V 1
+meme V 2
 by guadalupe ramirez
 text + images
 */
 
-var voiceImage;
-var hangImage;
+//var voiceImage;
+//var hangImage;
 
 function preload() {
     voiceImage = loadImage('voicememe.jpg');
@@ -14,24 +14,30 @@ function preload() {
 
 
 function setup() {
-    var canvas = createCanvas(840, 560);
-    canvas.drawingContext.miterLimit = 2;
+    createCanvas(600, 600);
 }
-
+    
 function draw() {
-    background(225, 52, 89);
-
+    background(0);
+    
+     if (mouseIsPressed) {
+        image(hangImage, 0, 0);
+    }  else {
+        image(voiceImage, 0, 0);
+    }
+}
+    
     /* image */
-    image(voiceImage, 110, 94);
+    //image(voiceImage, 110, 94);
 
     /*hang up button*/
-    image(hangImage, 176, 337);
+    //image(hangImage, 176, 337);
 
-    textSize(35);
-    fill('gold');
-    fill('gold');
-    stroke('black');
-    strokeWeight(10);
-    text('Professor: Okay guys, thats it for to...', 35, 60);
-    text('Me:', 35, 100);
+    //textSize(35);
+    //fill('gold');
+    //fill('gold');
+    //stroke('black');
+    //strokeWeight(10);
+    //text('Professor: Okay guys, thats it for to...', 35, 60);
+    //text('Me:', 35, 100);
 }

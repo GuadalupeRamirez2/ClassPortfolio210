c5b9daf671b1070961a17776ed8af93fa480ca25
GuadalupeRamirez2 <64633227+GuadalupeRamirez2@users.noreply.github.com>
2020-10-22 19:31:02 -0400

    done

--- Meme_v3/sketch.js
+++ Meme_v3/sketch.js
@@ -1,12 +1,14 @@
 /*
-meme V 2
+meme V 3
 by guadalupe ramirez
 text + images
 */
-
 var voiceImage;
 var hangImage;
 var exampleImage;
+var s = 1;
+var scaleSpeed =  0.01;
+
 
 function preload() {
     voiceImage = loadImage('voicememe.jpg');
@@ -22,12 +24,22 @@ function setup() {
 
 function draw() {
     background(225, 52, 89);
-
+    
+    translate(10, 5);
+    push();
+    scale(s);
+    s += scaleSpeed;
+    if (s>2 || s < 0.25) {
+        scaleSpeed *= -1;
+    }
+    
     if (mouseIsPressed) {
         image(exampleImage, 280, 15, 290, 530);
     } else {
         image(voiceImage, 110, 94);
         image(hangImage, 176, 337);
+        pop();
+        
         textSize(35);
         fill('gold');
         fill('gold');
@@ -37,5 +49,3 @@ function draw() {
         text('Me:', 35, 100);
     }
 }
-
-

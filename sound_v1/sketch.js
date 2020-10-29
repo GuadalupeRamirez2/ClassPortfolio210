03661a798dff54bb5d8f70cc3da17a93ff23b252
GuadalupeRamirez2 <64633227+GuadalupeRamirez2@users.noreply.github.com>
2020-10-29 19:07:11 -0400

    done

--- sound_v1/sketch.js
+++ sound_v1/sketch.js
@@ -2,7 +2,7 @@
 sound sampler v1 
 */
 
-var pacmanSound, pacman2Sound, pacman3Sound, pacman4Sound, pacman5Sound;
+var pacmanSound, pacman2Sound, pacman3Sound, pacman4Sound;
 var rapSound;
 
 function preload() {
@@ -10,7 +10,6 @@ function preload() {
     pacman2Sound = loadSound('pacman2.mp3');
     pacman3Sound = loadSound('pacman3.mp3');
     pacman4Sound = loadSound('pacman4.mp3');
-    pacman5Sound = loadSound('pacman5.mp3');
     rapSound = loadSound('cityrap.mp3');
 }
 
@@ -20,7 +19,6 @@ function setup() {
     pacman2Sound.playMode('restart');
     pacman3Sound.playMode('restart');
     pacman4Sound.playMode('restart');
-    pacman5Sound.playMode('restart');
 
 }
 
@@ -28,69 +26,84 @@ function draw() {
     background(220);
 
     if (rapSound.isPlaying()) {
-        background('purple');
+        background('blue');
     }
 
     if (pacmanSound.isPlaying()) {
-        fill('blue');
+        fill('gold');
         noStroke();
-      rect(50, 20,50,50, 10);
+        ellipse(100, 260, 50, 50, 10);
+        ellipse(200, 260, 50, 50, 10);
+        ellipse(300, 260, 50, 50, 10);
+        ellipse(400, 260, 50, 50, 10);
+        ellipse(500, 260, 50, 50, 10);
+        ellipse(600, 260, 50, 50, 10);
 
     }
     if (pacman2Sound.isPlaying()) {
-        fill('red');
+        fill('orange');
         noStroke();
-    
+        rect(100, 100, 50, 50, 10);
+        rect(200, 150, 50, 50, 10);
+        rect(300, 200, 50, 50, 10);
+        rect(400, 250, 50, 50, 10);
+        rect(500, 300, 50, 50, 10);
+        rect(600, 50, 50, 50, 10);
+
     }
     if (pacman3Sound.isPlaying()) {
         fill('pink');
         noStroke();
-      
+        rect(20, 100, 50, 50, 10);
+        rect(80, 150, 50, 50, 10);
+        rect(160, 200, 50, 50, 10);
+        rect(260, 250, 50, 50, 10);
+        rect(350, 300, 50, 50, 10);
+        rect(400, 50, 50, 50, 10);
 
-        if (pacman4Sound.isPlaying()) {
-            fill('cyan');
-            noStroke();
-         
-
-        }
-        if (pacman5Sound.isPlaying()) {
-            fill('orange');
-            noStroke();
-          
-        }
-        //visualize sound time
-        var currentTime = rapSound.currentTime();
-        var duration = rapSound.duration();
-        var timeElapsed = map(currentTime, 0, duration, 0, width);
-
-        fill('yellow');
+    }
+    if (pacman4Sound.isPlaying()) {
+        fill('cyan');
         noStroke();
-    }
+        rect(100, 160, 50, 50, 10);
+        rect(200, 160, 50, 50, 10);
+        rect(300, 160, 50, 50, 10);
+        rect(400, 160, 50, 50, 10);
+        rect(500, 160, 50, 50, 10);
+        rect(600, 160, 50, 50, 10);
 
-    function mousePressed() {
-        if (rapSound.isPlaying()) {
-            rapSound.pause();
-        } else {
-            rapSound.play();
-        }
-    }
-
-    function keyPressed() {
-        if (keyCode == 13) { //enter key
-            pacmanSound.play();
-        }
-        if (keyCode == 65) { //a key
-            pacman2Sound.play();
-        }
-        if (keyCode == 66) { //b key
-            pacman3Sound.play();
-        }
-        if (keyCode == 67) { //c key
-            pacman4Sound.play();
-        }
-        if (keyCode == 68) { //d key
-            pacman5Sound.play();
-        }
 
     }
+    //visualize sound time
+    var currentTime = rapSound.currentTime();
+    var duration = rapSound.duration();
+    var timeElapsed = map(currentTime, 0, duration, 0, width);
+
+    fill('yellow');
+    noStroke();
+    arc(timeElapsed, 255, 100, 100, 0.5, TWO_PI - 0.5, );
+}
+
+function mousePressed() {
+    if (rapSound.isPlaying()) {
+        rapSound.pause();
+    } else {
+        rapSound.play();
+    }
+}
+
+function keyPressed() {
+    if (keyCode == 13) { //enter key
+        pacmanSound.play();
+    }
+    if (keyCode == 65) { //a key
+        pacman2Sound.play();
+    }
+    if (keyCode == 66) { //b key
+        pacman3Sound.play();
+    }
+    if (keyCode == 67) { //c key
+        pacman4Sound.play();
+    }
+
 }

cb838cc5307488a70b492d3406549a199f9ff3c9
GuadalupeRamirez2 <64633227+GuadalupeRamirez2@users.noreply.github.com>
2020-11-03 19:56:44 -0500

    still working on it

--- sound_v2/sketch.js
+++ sound_v2/sketch.js
@@ -4,16 +4,20 @@ Interaction 2 - collision
 var currentColor;
 var p, o, c;
 var colorNumber = 0;
-var pacmanSound;
+var pacmanSound, pacman2Sound, pacman3Sound, rapSound;
 
 function preload() {
     pacmanSound = loadSound('pacman.mp3');
+    pacman2Sound = loadSound('pacman2.mp3');
+    pacman3Sound = loadSound('pacman3.mp3');
+    rapSound = loadSound('cityrap.mp3');
 }
 
 
 function setup() {
     createCanvas(640, 360);
     pacmanSound.playMode('restart');
+    rapSound.playMode('restart');
     p = color(172, 3, 174); //purple
     o = color(255, 165, 0); //orange
     c = color(0, 255, 255); //cyan
@@ -23,32 +27,39 @@ function setup() {
 function mousePressed() {
     //click pacman
     var d = dist(475, 220, mouseX, mouseY);
-    if (d < 50) { 
-        pacmanSound.play();
+    if (d < 50) {
+        rapSound.play();
     }
-     
-    
-    //click the ghost
-    if (mouseX > 100 && mouseX < 150 && mouseY > 200 && mouseY < 250) {
-        fill('blue');
-         // change the background color
-        colorNumber++;
-        if (colorNumber == 3) {
-            colorNumber = 0;
-        }
-        if (colorNumber==0){
-            currentColor=p;
-        } else if (colorNumber==1){
-            currentColor=o;
-        } else if (colorNumber==2){
-            currentColor=c;
-        }
+}
+//click the ghost
+if (mouseX > 100 && mouseX < 150 && mouseY > 200 && mouseY < 250) {
+    fill('blue');
+    // change the background color
+    colorNumber++;
+    if (colorNumber == 3) {
+        colorNumber = 0;
+    }
+    if (colorNumber == 0) {
+        currentColor = p;
+    } else if (colorNumber == 1) {
+        currentColor = o;
+    } else if (colorNumber == 2) {
+        currentColor = c;
     }
 }
 
 function draw() {
     background(currentColor);
 
+    button(40, 220, 50, pacmanSound);
+    button(240, 220, 50, pacman2Sound);
+    button(340, 220, 50, pacman3Sound);
+
+    var x = 100;
+    var y = 200;
+    var w = 50;
+    var h = 50;
+    ghostButton(100, 200, 50, 50, 10);
 
     // pacman button
     fill('yellow');
@@ -58,8 +69,32 @@ function draw() {
     }
     arc(475, 220, 100, 100, 180, PI + TWO_PI, PIE);
 
-    //ghost button
-    fill('red');
 
-    rect(100, 200, 50, 50, 10);
+    function ghostButton(x, y, w, h) {
+        //detect mouse inside rect
+        if (mouseX > x && mouseX < x + w && mouseY > y && mouseY < y + h) {
+            fill('blue');
+        } else {
+            fill('red');
+        }
+        rect(x, y, w, h, 10);
+    }
+
+    //click coin
+    function button(x, y, s, sound) {
+        var d = dist(x, y, mouseX, mouseY);
+        //calculate distance
+        if (d < s / 2) {
+            //inside the button
+            fill('yellow');
+
+            if (mouseIsPressed && !sound.isPlaying()) {
+                sound.play();
+            }
+        } else {
+            //outside the button
+            fill('gold');
+        }
+        ellipse(x, y, s, 50, 10);
+    }
 }

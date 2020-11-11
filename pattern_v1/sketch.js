bbb1334df72da297561008ec84cf94839b812e15
GuadalupeRamirez2 <64633227+GuadalupeRamirez2@users.noreply.github.com>
2020-11-10 19:57:29 -0500

    almost done

--- pattern_v1/sketch.js
+++ pattern_v1/sketch.js
@@ -7,8 +7,12 @@ function setup() {
     createCanvas(640, 360);
 }
 
-function draw() {
-    background('lightblue');
+function mousePressed() {
+    pattern();
+}
+
+function pattern() {
+    background('cyan');
 
 
 
@@ -23,35 +27,40 @@ function draw() {
 
     }
 
+    // buildings
+    for (let a = 0; a < width; a += 90) {
+        fill('plum');
+        let y = random(50, 280);
+        rect(a + 100, y, 80, 500);
+        fill('lightblue');
+        rect(a + 50, y - 25, 40, 300);
+        fill('pink')
+        rect(a, y, 60, 250);
+    }
+
     // light poles
     for (let a = 30; a < width; a += 140) {
+        fill('black');
         rect(a + 100, 100, 10, 220); //poles
         fill('yellow');
-        ellipse(a-36, 90, 35);//lightbowls
-        fill('brown');
+        ellipse(a - 36, 90, 35); //lightbowls
     }
 
     //train wheels
     for (let a = -90; a < width; a += 150) {
         noStroke();
+        fill('black');
         ellipse(a + 10, 330, 20, 20); //wheels
-        fill('black');
         ellipse(a + 30, 330, 20, 20); //wheels
-        fill('black');
         ellipse(a + 110, 330, 20, 20); //wheels
-        fill('black');
         ellipse(a + 130, 330, 20, 20); //wheels
-        fill('black');
-    }
 
-
-    // train
-    for (let a = -90; a < width; a += 150) {
+        //train
         stroke('black');
         strokeWeight(4);
-        rect(a, 240, 140, 90, 10); //windows
-        fill('gold');
-        rect(a, 275, 138, 30, 10); //wagons
         fill('orange');
+        rect(a + 1, 240, 150, 90, 10); //wagons
+        fill('gold');
+        rect(a + 10, 275, 130, 30, 10); //windows
     }
 }

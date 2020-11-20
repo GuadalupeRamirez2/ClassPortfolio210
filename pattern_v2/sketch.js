4c96e8a4635fdb10c0a1fa49c4d99060250b16c4
GuadalupeRamirez2 <64633227+GuadalupeRamirez2@users.noreply.github.com>
2020-11-19 19:35:57 -0500

    in progress

--- pattern_v2/sketch.js
+++ pattern_v2/sketch.js
@@ -1,10 +1,36 @@
 /*
- pattern version 2
- javascript loops
+ Generative pattern version 2
+
 */
 
 function setup() {
     createCanvas(640, 360);
+    pattern();
 }
 
-function mousePressed() {}
+function mousePressed() {
+    pattern();
+}
+
+function pattern() {
+    background(20);
+
+    let w = random(30,50);
+    let h = w;
+
+    for (let a = 0; a < width + w; a += w) {
+        for (let b = 0; b < height + h; b += h) {
+            
+            let r  = random(255);
+            let g  = random(255);
+            let b  = random(255);
+            
+            stroke(r,g,b);
+            strokeWeight(8);
+            noFill();
+            line(a, b, a + w, b, ); //cyan lines
+            line(a, b + h / 2, a + w, b + h / 2, ); // red  lines
+            line(a, b, a, b + h, ); //pink lines
+        }
+    }
+}
